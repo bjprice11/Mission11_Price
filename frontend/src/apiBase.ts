7a69bdd2e2cce6361ec7b2deceea6cf25e5ba8cf
bjprice11 <bjprice110@gmail.com>
2026-04-01 18:14:18 -0600

    setting up frontend and backend connections

--- frontend/src/apiBase.ts
+++ frontend/src/apiBase.ts
@@ -4,4 +4,4 @@
 // (see frontend/vite.config.ts). So we call "/api/Books" which becomes "https://localhost:7218/Books".
 //
 // In production build, there is no Vite proxy — you'd point this at your real API host.
-export const apiBase = import.meta.env.DEV ? "/api" : "https://localhost:7218";
+export const apiBase = import.meta.env.DEV ? "/api" : "https://mission13backend-h9ehe7h5csa8g4da.francecentral-01.azurewebsites.net";
